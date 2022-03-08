19d7540b7f818f354009616e5f89bd131815aa52
Dan Hert <hertdani@fel.cvut.cz>
2022-03-08 13:36:10 +0100

    update

--- main/main.c
+++ main/main.c
@@ -10,6 +10,7 @@
 #include "esp_log.h"
 #include "soc/uart_struct.h"
 #include "ucam_iii.h"
+#include "uart_glue.h"
 
 
 #define ECHO_TEST_TXD (17)
@@ -28,20 +29,22 @@
 
 void app_main(void) {
 
-  const int     uart_num    = UART_NUM_1;
-  uart_config_t uart_config = {
-      .baud_rate           = 921600,
-      /* .baud_rate           = 115200, */
-      .data_bits           = UART_DATA_8_BITS,
-      .parity              = UART_PARITY_DISABLE,
-      .stop_bits           = UART_STOP_BITS_1,
-      .flow_ctrl           = UART_HW_FLOWCTRL_DISABLE,
-      .rx_flow_ctrl_thresh = 122,
-  };
+  /* const int     uart_num    = UART_NUM_1; */
+  /* uart_config_t uart_config = { */
+  /*     .baud_rate           = 921600, */
+  /*     /1* .baud_rate           = 115200, *1/ */
+  /*     .data_bits           = UART_DATA_8_BITS, */
+  /*     .parity              = UART_PARITY_DISABLE, */
+  /*     .stop_bits           = UART_STOP_BITS_1, */
+  /*     .flow_ctrl           = UART_HW_FLOWCTRL_DISABLE, */
+  /*     .rx_flow_ctrl_thresh = 122, */
+  /* }; */
 
-  uart_param_config(uart_num, &uart_config);
-  uart_set_pin(uart_num, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
-  uart_driver_install(uart_num, BUF_SIZE * 2, 0, 0, NULL, 0);
+  /* uart_param_config(uart_num, &uart_config); */
+  /* uart_set_pin(uart_num, ECHO_TEST_TXD, ECHO_TEST_RXD, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE); */
+  /* uart_driver_install(uart_num, BUF_SIZE * 2, 0, 0, NULL, 0); */
+
+  init_uart(921600, 1024);
 
   uint8_t* datakeeper = (uint8_t*)malloc(38400 * sizeof(uint8_t)); // simulation of image storage
 
@@ -62,9 +65,9 @@ void app_main(void) {
   }
 
  /* printf("sending init\n"); */
- /*  /1* sendCommand(INIT_ID, 0x00, INIT_P2_JPEG, 0x07, INIT_P4_JPEG_320X240, 100); *1/ */
+ /*  sendCommand(INIT_ID, 0x00, INIT_P2_JPEG, 0x07, INIT_P4_JPEG_320X240, 100); */
  /*  /1* sendCommand(INIT_ID, 0x00, INIT_P2_JPEG, 0x07, INIT_P4_JPEG_640X480, 100); *1/ */
- /*  sendCommand(INIT_ID, 0x00, INIT_P2_JPEG, 0x07, INIT_P4_JPEG_160X128, 100); */
+ /*  /1* sendCommand(INIT_ID, 0x00, INIT_P2_JPEG, 0x07, INIT_P4_JPEG_160X128, 100); *1/ */
  /*  vTaskDelay(100 / portTICK_PERIOD_MS); */
 
  /*  printf("sending package size 512 bytes\n"); */
@@ -76,9 +79,11 @@ void app_main(void) {
  /*  vTaskDelay(1000 / portTICK_PERIOD_MS); */
 
  /*  printf("requesting image\n"); */
+
  /*  uint32_t image_size = request_image(GET_PICTURE_P1_SNAPSHOT_MODE); */
 
  /*  printf("requesting image compl %i\n" , image_size); */
+
  /*  bool got_image = receive_jpeg_image(image_size, 512, datakeeper);  // packet size hardcoded for now */
 
  /*  vTaskDelay(2000 / portTICK_PERIOD_MS); */
@@ -99,8 +104,8 @@ void app_main(void) {
   vTaskDelay(1000 / portTICK_PERIOD_MS);
 
   bool got_image = receive_raw_image(1024, datakeeper);  // packet size hardcoded for now
+  /* int     len = uart_read(datakeeper, 40000, 1000 / portTICK_RATE_MS); */
 
-  int     len = uart_read_bytes(UART_NUM_1, datakeeper, 40000, 1000 / portTICK_RATE_MS);
   if (got_image) {
     printf("sssss"); //synchronization message for the python image receiver
     print_buffer_as_hex(datakeeper, 19200);
@@ -115,4 +120,3 @@ void app_main(void) {
 }
 
 //}
-
